3a75f9fcdda2c4c2e78ae97f384b4c782c42550e
Andrew Leung <1062708758@qq.com>
2023-02-20 10:45:36 +0800

    feat:base MainGrid

---
+++ src/components/Home/MainGrid.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="main-grid" class="gap-3">
+    <div id="cate-items-menu" style="grid-area: cim" class="bg-white"></div>
+    <div id="main-slider" style="grid-area: ms" class="bg-white"></div>
+    <div id="focus-items-0" style="grid-area: fi0" class="bg-white"></div>
+    <div id="focus-items-1" style="grid-area: fi1" class="bg-white"></div>
+    <div id="focus-items-2" style="grid-area: fi2" class="bg-white"></div>
+    <div id="other" style="grid-area: o" class="bg-white"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+#main-grid {
+  --width: 190px;
+  --height: 150px;
+  display: grid;
+
+  min-width: 1000px;
+
+  grid-template-areas:
+    'cim ms ms ms fi0 o'
+    'cim ms ms ms fi1 o'
+    'cim ms ms ms fi2 o';
+
+  grid-template-columns: repeat(6, var(--width));
+  grid-template-rows: repeat(3, var(--height));
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import HomeHeader from '@/components/Home/HomeHeader.vue';
+import MainGrid from '@/components/Home/MainGrid.vue';
 
 </script>
 
 <template>
   <main>
     <HomeHeader />
+    <div class="pt-3 w-fit m-auto">
+      <MainGrid />
+    </div>
   </main>
 </template>
