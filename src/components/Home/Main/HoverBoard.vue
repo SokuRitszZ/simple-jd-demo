8f3c6193535c8f8c32d75350bb24340fcb668d98
Andrew Leung <1062708758@qq.com>
2023-02-20 19:23:41 +0800

    update:MainGrid HoverBoard

---
+++ src/components/Home/Main/HoverBoard.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+// @ts-ignore
+import faker from 'faker';
+import randomPic from '@/utils/randomPic';
+
+type typeOptions = {
+  title: string;
+  options: string[];
+};
+
+const optionsTop = ref<string[]>([]);
+const optionsMap = ref<typeOptions[]>(new Array(10).fill(0).map(() => {
+  return {
+    title: faker.lorem.word(),
+    options: new Array(10).fill(0).map(() => faker.lorem.word()),
+  }
+}));
+
+type PropsType = {
+  keyword: string;
+};
+
+onMounted(() => {
+  optionsTop.value = new Array(5).fill(0).map(() => faker.lorem.word());
+});
+
+const props = defineProps<PropsType>();
+</script>
+
+<template>
+  <div class="w-full h-full flex">
+    <div class="w-[600px]">
+      <nav class="flex gap-2">
+        <div
+          :key="o"
+          v-for="o in optionsTop"
+          class="bg-black text-white px-2 py-1 hover:bg-[#C81623] cursor-pointer"
+        >
+          {{ o }}
+        </div>
+      </nav>
+      <div class="w-full grid grid-cols-9 gap-4 pt-5" :key="om.title" v-for="om of optionsMap">
+        <div class="col-span-2 text-right font-bold cursor-pointer hover:text-[#C81623]">
+          {{ om.title }}
+        </div>
+        <div class="col-span-7 flex gap-2 flex-wrap text-[#666666]">
+          <div :key="o" v-for="o in om.options" class="hover:text-[#C81623] cursor-pointer">{{ o }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="w-[200px]">
+      <img :src="randomPic()" alt="brand_pic" class="w-full max-h-[300px]" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
--- src/components/Home/MainGrid.vue
+++ src/components/Home/MainGrid.vue
@@ -1,9 +1,36 @@
 <script setup lang="ts">
+import HoverBoard from '@/components/Home/Main/HoverBoard.vue';
+// @ts-ignore
+import faker from 'faker';
+
+const keywords = new Array(15).fill(0).map(() => {
+  return new Array(3).fill(0).map(() => faker.lorem.word(4));
+});
 </script>
 
 <template>
   <div id="main-grid" class="gap-3">
-    <div id="cate-items-menu" style="grid-area: cim" class="bg-white"></div>
+    <div
+      id="cate-items-menu"
+      style="grid-area: cim"
+      class="bg-white pt-3 relative"
+    >
+      <div
+        :key="i"
+        v-for="(ws, i) of keywords"
+        class="option pl-5 py-1 text-sm hover:bg-gray-300 transition-colors"
+      >
+        <span :key="i" v-for="(w, i) of ws">
+          <span v-if="i" class="px-1">/</span>
+          <span class="hover:text-[#C81623] cursor-pointer">{{ w }}</span>
+        </span>
+        <div
+          class="board absolute left-full top-0 bg-white border-[1px] border-gray-100 p-4 min-w-[800px] shadow-lg hidden"
+        >
+          <HoverBoard :keyword="'' + i" />
+        </div>
+      </div>
+    </div>
     <div id="main-slider" style="grid-area: ms" class="bg-white"></div>
     <div id="focus-items-0" style="grid-area: fi0" class="bg-white"></div>
     <div id="focus-items-1" style="grid-area: fi1" class="bg-white"></div>
@@ -26,5 +53,9 @@
 
   grid-template-columns: repeat(6, auto);
   grid-template-rows: repeat(3, var(--height));
+}
+
+.option:hover > .board {
+  display: block;
 }
 </style>
