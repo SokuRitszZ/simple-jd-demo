b3cf8ffb38a5053f3c360529dc4515f4d5e48289
Andrew Leung <1062708758@qq.com>
2023-02-20 15:47:49 +0800

    update:GlobalNav hover dropdown

--- src/components/GlobalNav.vue
+++ src/components/GlobalNav.vue
@@ -1,25 +1,73 @@
 <script setup lang="ts">
+// @ts-ignore
+import faker from "faker";
+
 const options = ['我的京东', '企业采购', '商家服务', '网站导航'];
+
+function getSubOptions() {
+  return new Array(Math.random() * 20 >>> 0).fill(0).map(() => faker.lorem.word(4))
+}
+
 </script>
 
 <template>
   <nav id="home-nav">
-    <div class="bg-[#E3E4E5] p-1">
-      <div class="min-w-[1000px] w-fit m-auto flex justify-between text-sm py-1">
-        <div id="header-location" class="text-[#999999] flex items-center gap-1">
+    <div class="bg-[#E3E4E5]">
+      <div class="w-[1200px] m-auto flex justify-between text-sm">
+        <div
+          id="header-location"
+          class="text-[#999999] flex items-center gap-1 py-1"
+        >
           <div class="inline-block w-1 h-1 rounded-full p-2 bg-[#F10215]"></div>
-          <span>
-            location
-          </span>
+          <span> location </span>
         </div>
-        <div id="header-options">
-          <span class="text-sm px-2 text-[#999999] hover:text-[#F30213]" :class="[i ? 'border-l-[1px] border-gray-300' : '']" :key="o" v-for="(o, i) of options">
-            {{ o }}
-          </span>
+        <div id="header-options" class="text-sm text-[#999999] py-1 flex items-center">
+          <div class="nav-item border-none">你好，请登录</div>
+          <div class="ml-1 mr-4 text-[#F10215] cursor-pointer">免费注册</div>
+          <div class="nav-item pr-1">我的订单</div>
+          <div class="nav-item relative" :key="o" v-for="o of options">
+            <div class="nav-item-label">
+              {{ o }}
+            </div>
+            <div class="dropdown absolute hidden w-[300px] grid-cols-3">
+              <span :key="oo" v-for="oo in getSubOptions()" class="sub-option">{{ oo }}</span>
+            </div>
+          </div>
+          <div class="nav-item px-1">手机京东</div>
+          <div class="nav-item px-1">网站无障碍</div>
         </div>
       </div>
     </div>
   </nav>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.nav-item {
+  @apply leading-[28px] h-[28px] cursor-pointer text-[#999999] hover:text-[#F30213];
+  .nav-item-label {
+    @apply px-1;
+  }
+}
+
+.nav-item-label:hover {
+  @apply bg-white z-20 w-full h-full;
+  &+.dropdown {
+    display: block;
+    left: 0;
+    top: 100%;
+    @apply bg-white border-[1px] border-t-0 border-gray-200 z-10 p-4 grid;
+  }
+
+}
+
+.dropdown:hover {
+  display: block;
+  left: 0;
+  top: 100%;
+  @apply cursor-default bg-white border-[1px] border-t-0 border-gray-200 z-10 p-4 grid;
+}
+
+.sub-option {
+  @apply text-[#999999] hover:text-[#F30213] cursor-pointer;
+}
+</style>
--- src/components/Home/HomeHeader.vue
+++ src/components/Home/HomeHeader.vue
@@ -24,42 +24,47 @@ const navItems = [
 <template>
   <header
     id="home-header"
-    class="bg-white border-b-[1px] flex justify-center items-center gap-10"
+    class="bg-white border-b-[1px]"
   >
-    <div id="logo" class="w-[120px] h-[120px] bg-[#E21D17]"></div>
-    <div id="header-middle">
-      <div id="search" class="pt-6 w-fit relative flex items-center">
-        <input
-          placeholder="Shopping"
-          id="search-input"
-          class="border-[#E21D17] border-2 w-[530px] px-5 outline-none py-2 text-sm"
-          type="text"
-        >
-        <span class="absolute left-[500px] text-[#B9B9B9] hover:text-[#D54F58] cursor-pointer">图</span>
-        <button
-          class="bg-[#E1251B] border-[#E1251B] border-2 font-bold text-white px-3 py-2 text-center text-sm min-w-[60px]"
-        >
-          搜
-        </button>
-      </div>
-      <div id="hot-words" class="flex gap-3 text-sm pt-1">
-        <span
-          :class="[
-            i ? 'text-[#B4B4B4]' : 'text-[#E1251B]',
-            'hover:text-[#C81623] cursor-pointer',
-          ]"
-          :key="w"
-          v-for="(w, i) in hotWords"
-          >{{ w }}</span
-        >
-      </div>
-      <div id="nav-items" class="pt-6">
-        <a
-          class="cursor-pointer transition-colors hover:text-[#D6515B] mx-3 leading-[40px]"
-          :key="n"
-          v-for="n in navItems"
-          >{{ n }}</a
-        >
+    <div class="w-[1200px] m-auto flex justify-center items-center gap-10">
+      <div id="logo" class="w-[120px] h-[120px] bg-[#E21D17]"></div>
+      <div id="header-middle">
+        <div id="search" class="pt-6 w-fit relative flex items-center">
+          <input
+            placeholder="Shopping"
+            id="search-input"
+            class="border-[#E21D17] border-2 w-[530px] px-5 outline-none py-2 text-sm"
+            type="text"
+          />
+          <span
+            class="absolute left-[500px] text-[#B9B9B9] hover:text-[#D54F58] cursor-pointer"
+            >图</span
+          >
+          <button
+            class="bg-[#E1251B] border-[#E1251B] border-2 font-bold text-white px-3 py-2 text-center text-sm min-w-[60px]"
+          >
+            搜
+          </button>
+        </div>
+        <div id="hot-words" class="flex gap-3 text-sm pt-1">
+          <span
+            :class="[
+              i ? 'text-[#B4B4B4]' : 'text-[#E1251B]',
+              'hover:text-[#C81623] cursor-pointer',
+            ]"
+            :key="w"
+            v-for="(w, i) in hotWords"
+            >{{ w }}</span
+          >
+        </div>
+        <div id="nav-items" class="pt-6">
+          <a
+            class="cursor-pointer transition-colors hover:text-[#D6515B] mx-3 leading-[40px]"
+            :key="n"
+            v-for="n in navItems"
+            >{{ n }}</a
+          >
+        </div>
       </div>
     </div>
   </header>
