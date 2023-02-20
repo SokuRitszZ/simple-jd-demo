dac12f2bb028737b8729ff0b29672dedc6e950e3
Andrew Leung <1062708758@qq.com>
2023-02-20 11:24:28 +0800

    feat:base MiaoShaGrid

---
+++ src/components/Home/MiaoShaGrid.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const items = [0, 1, 2, 3];
+</script>
+
+<template>
+  <div id="miaosha-grid" class="gap-3 grid h-[260px]">
+    <div id="miaosha-board" class="bg-white col-span-1"></div>
+    <div id="slider-items" class="bg-white col-span-4 grid grid-cols-4">
+      <div :key="i" v-for="i in items" :class="[i ? 'border-l-[1px] border-gray-100' : '']">{{ i }}</div>
+    </div>
+    <div id="seckill-brand" class="bg-white col-span-1"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+#miaosha-grid {
+  grid-template-columns: repeat(6, 190px);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import HomeHeader from '@/components/Home/HomeHeader.vue';
 import MainGrid from '@/components/Home/MainGrid.vue';
+import MiaoShaGrid from '@/components/Home/MiaoShaGrid.vue';
 
 </script>
 
@@ -9,6 +10,9 @@ import MainGrid from '@/components/Home/MainGrid.vue';
     <HomeHeader />
     <div class="pt-3 w-fit m-auto">
       <MainGrid />
+    </div>
+    <div class="pt-3 w-fit m-auto">
+      <MiaoShaGrid />
     </div>
   </main>
 </template>
