c15f6cb4ed622af7554410c4cd9ed08ece5306e4
Andrew Leung <1062708758@qq.com>
2023-02-22 09:12:08 +0800

    update:use radio in JoySeek nav

--- src/components/Home/Spec/JoySeek.vue
+++ src/components/Home/Spec/JoySeek.vue
@@ -13,19 +13,25 @@ type typeItem = {
 };
 
 const navs = ref<string[]>(new Array(4).fill(0).map(() => faker.lorem.word(4)));
+const navSelected = ref<string>('');
 
-const itemsThree = ref<typeItem[]>([]);
+const itemsThree = ref<{ [key: string]: typeItem[] }>({});
+const listShown = ref<typeItem[]>([]);
 
 function select(option: string) {
-  console.log(option);
-  itemsThree.value = new Array(3).fill(0).map(() => {
-    return {
-      id: (Math.random() * 10000) >>> 0,
-      pic: randomPic(),
-      price: +(Math.random() * 1000).toFixed(2),
-      title: faker.lorem.sentence(10),
-    };
-  });
+  navSelected.value = option;
+  const list =
+    itemsThree.value[option] ||
+    new Array(3).fill(0).map(() => {
+      return {
+        id: (Math.random() * 10000) >>> 0,
+        pic: randomPic(),
+        price: +(Math.random() * 1000).toFixed(2),
+        title: faker.lorem.sentence(10),
+      };
+    });
+  itemsThree.value[option] = list;
+  listShown.value = list;
 }
 </script>
 
@@ -37,28 +43,51 @@ function select(option: string) {
         href="#"
         :key="nv"
         v-for="nv of navs"
-        class="bg-[#f6f6f6] w-1/4 text-center text-[#999999] rounded-full text-sm hover:bg-[#e1251b] hover:text-white"
-        >{{ nv }}</a
+        class="w-1/4"
       >
+        <label :for="`lb-${nv}`" class="nav pointer-events-none">
+          {{ nv }}
+        </label>
+        <input
+          name="option"
+          :id="`lb-${nv}`"
+          type="radio"
+          class="hidden"
+          :checked="navSelected === nv"
+        />
+      </a>
     </nav>
     <div class="grow pt-3 flex flex-col gap-3">
-      <a href="#" :key="it.id" v-for="it in itemsThree" class="flex gap-2">
-        <img :src="it.pic" class="w-[70px] h-[70px]" alt="" />
-        <div class="w-[150px]">
-          <div
-            class="overflow-hidden text-ellipsis whitespace-nowrap w-full text-sm"
-          >
-            {{ it.title }}
+      <keep-alive>
+        <a href="#" :key="it.id" v-for="it in listShown" class="flex gap-2">
+          <img :src="it.pic" class="w-[70px] h-[70px]" alt="" />
+          <div class="w-[150px]">
+            <div
+              class="overflow-hidden text-ellipsis whitespace-nowrap w-full text-sm"
+            >
+              {{ it.title }}
+            </div>
+            <div class="text-sm text-[#e1251b]">
+              <span class="text-[10px]"> ￥ </span
+              ><span class="text-lg">{{
+                it.price.toString().split('.')[0]
+              }}</span
+              ><span>.{{ it.price.toString().split('.')[1] }}</span>
+            </div>
           </div>
-          <div class="text-sm text-[#e1251b]">
-            <span class="text-[10px]"> ￥ </span
-            ><span class="text-lg">{{ it.price.toString().split('.')[0] }}</span
-            ><span>.{{ it.price.toString().split('.')[1] }}</span>
-          </div>
-        </div>
-      </a>
+        </a>
+      </keep-alive>
     </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+a:has(.nav) {
+  width: inherit;
+  @apply bg-[#f6f6f6] text-center text-[#999999] rounded-full text-sm;
+}
+
+a:has(input:checked) {
+  @apply bg-[#e1251b] text-white;
+}
+</style>
